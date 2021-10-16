6d0750effbc95197730cdcb35864e188f3b63b86
LGregg11 <L.Gregory11@hotmail.co.uk>
2021-10-16 15:45:28 +0100

    Add NUnit test project to begin TDD

---
+++ F1TelemetryAppTests/UdpTelemetryFeedTests.cs
@@ -0,0 +1,50 @@
+namespace F1TelemetryAppTests
+{
+    using NUnit.Framework;
+    using System.Threading.Tasks;
+    using UdpTelemetryFeed;
+
+    public class UdpTelemetryFeedTests
+    {
+        private UdpTelemetryFeed cut;
+        private int portMock = 10101;
+
+        [SetUp]
+        public void Setup()
+        {
+            Task.Delay(500);
+            cut = new UdpTelemetryFeed(portMock);
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            if (cut != null)
+                cut.Stop();
+        }
+
+        [Test]
+        public void Start_ShouldStartTelemetryThread()
+        {
+            // Act
+            // Arrange
+            cut.Start();
+
+            // Assert
+            Assert.IsTrue(cut.ListenerThread.IsAlive);
+            Assert.AreEqual("Telemetry Listener Thread", cut.ListenerThread.Name);
+        }
+
+        [Test]
+        public void Start_ShouldStartUdpClient()
+        {
+            // Act
+            // Arrange
+            cut.Start();
+
+            // Assert
+            Assert.IsNotNull(cut.Client);
+            Assert.IsTrue(cut.Client.Client.Connected);
+        }
+    }
+}
