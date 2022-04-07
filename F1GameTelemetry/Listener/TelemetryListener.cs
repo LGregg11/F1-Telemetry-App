eee64e7f16f38e939f464dca978d5703ea16e400
Luke Gregory <L.Gregory11@hotmail.co.uk>
2022-04-07 12:25:08 +0100

    Fix telemetry listener (with a join...)

--- F1GameTelemetry/Listener/TelemetryListener.cs
+++ F1GameTelemetry/Listener/TelemetryListener.cs
@@ -1,5 +1,7 @@
 ﻿namespace F1GameTelemetry.Listener
 {
+    using log4net;
+
     using System;
     using System.Net;
     using System.Net.Sockets;
@@ -14,9 +16,12 @@
         public TelemetryListener(int port)
         {
             this.port = port;
+            Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         }
 
         public event TelemetryEventHandler TelemetryReceived;
+
+        public ILog Log { get; set; }
 
         public int Port => port;
 
@@ -27,28 +32,46 @@
         public void Start()
         {
             if (ListenerThread != null && ListenerThread.IsAlive)
+            {
+                Log?.Debug("Client already started");
                 return;
+            }
 
-            if (Client == null)
-                client = new UdpClient(Port);
-
+            if (Client != null)
+            {
+                Log?.Debug("Client already started");
+                return;
+            }
+            
+            client = new UdpClient(Port);
             listenerThread = new Thread(new ThreadStart(TelemetrySubscriber))
             {
                 Name = "Telemetry Listener Thread"
             };
+
+            Log?.Info("Starting Telemetry listener");
             listenerThread.Start();
         }
 
         public void Stop()
         {
             if (ListenerThread == null || !ListenerThread.IsAlive)
+            {
+                Log?.Debug("Thread is not active");
                 return;
+            }
             
-            if (Client != null)
-                client.Close();
-            listenerThread.Abort();
-            listenerThread.Join(5000);
+            if (Client == null)
+            {
+                Log?.Debug("Client is null");
+                return;
+            }
+
+            Log?.Info("Stopping Telemetry listener");
+            client.Close();
+            listenerThread.Join();
             listenerThread = null;
+            client = null;
         }
 
         public void TelemetrySubscriber()
@@ -61,11 +84,12 @@
                 {
                     byte[] receiveBytes = client.Receive(ref ep);
                     if (receiveBytes != null && receiveBytes.Length > 0)
-                        TelemetryReceived(this, new TelemetryEventArgs(receiveBytes));
+                        TelemetryReceived?.Invoke(this, new TelemetryEventArgs(receiveBytes));
                 }
-                catch (SocketException ex)
+                catch (SocketException)
                 {
-                    Console.WriteLine(ex.Message);
+                    Log?.Debug("Closing TelemetrySubscriber");
+                    break;
                 }
             }
         }
