51bb0a8dc68313c8f4c6688948c8614b7c4b1492
Luke Gregory <L.Gregory11@hotmail.co.uk>
2022-04-09 21:48:33 +0100

    F1-2: Add Session packet & Edit plural enum names

---
+++ F1TelemetryApp/Model/SessionMessage.cs
@@ -0,0 +1,14 @@
+﻿namespace F1TelemetryApp.Model
+{
+    using F1GameTelemetry.Packets.Enums;
+
+    public struct SessionMessage
+    {
+        public Track Track { get; set; }
+        public Weather Weather { get; set; }
+        public int TotalLaps { get; set; }
+        public sbyte TrackTemperature { get; set; }
+        public sbyte AirTemperature { get; set; }
+        public byte AiDifficulty { get; set; }
+    }
+}
