06c9ad3428c7e012b7be4cf36788053a746599e4
Luke Gregory <L.Gregory11@hotmail.co.uk>
2022-06-26 11:30:22 +0100

    F1-44: Create TelemetryGraph user control

---
+++ srs/F1TelemetryApp/Controls/TelemetryGraph.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace F1TelemetryApp.Controls;
+
+using System.Windows;
+using System.Windows.Controls;
+
+/// <summary>
+/// Interaction logic for TelemetryGraphControl.xaml
+/// </summary>
+public partial class TelemetryGraph : UserControl
+{
+    public TelemetryGraph()
+    {
+        InitializeComponent();
+    }
+
+    public string DataType { get; set; }
+
+    public string DataValue
+    {
+        get { return (string)GetValue(DataValueProperty); }
+        set { SetValue(DataValueProperty, value); }
+    }
+
+    public static readonly DependencyProperty DataValueProperty =
+        DependencyProperty.Register("DataValue", typeof(string), typeof(TelemetryGraph), new PropertyMetadata(null));
+
+}
