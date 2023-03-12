15453b219442220671a83b218a1657f84dfa19c3
Luke Gregory <L.Gregory11@hotmail.co.uk>
2023-03-12 11:31:52 +0000

    F1-51: Format TelemetryReader2021 syntax

--- src/F1GameTelemetry/Readers/F12021/TelemetryReader2021.cs
+++ src/F1GameTelemetry/Readers/F12021/TelemetryReader2021.cs
@@ -1,13 +1,14 @@
 ﻿namespace F1GameTelemetry.Readers.F12021;
 
-using F1GameTelemetry.Packets.F12021;
+using F1GameTelemetry.Converters;
 using F1GameTelemetry.Enums;
 using F1GameTelemetry.Listener;
+using F1GameTelemetry.Packets;
+using F1GameTelemetry.Packets.F12021;
+
 using System.Linq;
 using System.Threading.Tasks;
 using System.Collections.Generic;
-using F1GameTelemetry.Packets;
-using F1GameTelemetry.Converters;
 
 public class TelemetryReader2021 : BaseTelemetryReader
 {
@@ -47,87 +48,51 @@ public class TelemetryReader2021 : BaseTelemetryReader
     }
 
     public override string Name => "F1 2021 Telemetry Reader";
-
     public override bool IsSupported => true;
 
     public override IPacket HeaderPacket { get; }
-
     public override IPacket MotionPacket { get; }
-
     public override IPacket CarTelemetryPacket { get; }
-
     public override IPacket CarStatusPacket { get; }
-
     public override IPacket FinalClassificationPacket { get; }
-
     public override IPacket LapDataPacket { get; }
-
     public override IPacket SessionPacket { get; }
-
     public override IPacket ParticipantPacket { get; }
-
     public override IPacket SessionHistoryPacket { get; }
-
     public override IPacket LobbyInfoPacket { get; }
-
     public override IPacket CarDamagePacket { get; }
-
     public override IPacket CarSetupPacket { get; }
 
     public override void OnTelemetryReceived(object sender, TelemetryEventArgs e)
     {
         Header header = Converter.BytesToPacket<Header>(e.Message);
-        Task headerTask = new Task(() => HeaderPacket?.ReceivePacket(e.Message));
+        Task headerTask = new(() => HeaderPacket?.ReceivePacket(e.Message));
         headerTask.RunSynchronously();
 
         byte[] remainingPacket = e.Message.Skip(HeaderPacketSize).ToArray();
-        Task remainingTask = new Task(() => RaiseEventHandler((PacketId)header.packetId, remainingPacket));
+        Task remainingTask = new(() => RaiseEventHandler((PacketId)header.packetId, remainingPacket));
         remainingTask.RunSynchronously();
     }
 
     public object? GetEvent(byte[] remainingPacket)
     {
         EventType eventType = GetEventType(remainingPacket);
-        switch (eventType)
+        return eventType switch
         {
-            case EventType.BUTN:
-                return Converter.BytesToPacket<Buttons>(remainingPacket);
-            case EventType.FLBK:
-                return Converter.BytesToPacket<Flashback>(remainingPacket);
-            case EventType.STLG:
-                return Converter.BytesToPacket<StartLights>(remainingPacket);
-            case EventType.LGOT:
-                return Converter.BytesToPacket<StartLights>(remainingPacket);
-            case EventType.SPTP:
-                return Converter.BytesToPacket<SpeedTrap>(remainingPacket);
-            case EventType.FTLP:
-                return Converter.BytesToPacket<FastestLap>(remainingPacket);
-            case EventType.TMPT:
-                return Converter.BytesToPacket<TeamMateInPits>(remainingPacket);
-            case EventType.RCWN:
-                return Converter.BytesToPacket<RaceWinner>(remainingPacket);
-            case EventType.RTMT:
-                return Converter.BytesToPacket<Retirement>(remainingPacket);
-            case EventType.PENA:
-                return Converter.BytesToPacket<Penalty>(remainingPacket);
-            case EventType.SGSV:
-                return Converter.BytesToPacket<StopGoPenaltyServed>(remainingPacket);
-            case EventType.DTSV:
-                return Converter.BytesToPacket<DriveThroughPenaltyServed>(remainingPacket);
-            case EventType.SSTA:
-            // Session Started
-            case EventType.SEND:
-            // Session End
-            case EventType.DRSE:
-            // DRS Enabled
-            case EventType.DRSD:
-            // DRS Disabled
-            case EventType.CHQF:
-            // Chequered Flag
-            case EventType.UNKNOWN:
-            default:
-                return null;
-        }
+            EventType.BUTN => Converter.BytesToPacket<Buttons>(remainingPacket),
+            EventType.FLBK => Converter.BytesToPacket<Flashback>(remainingPacket),
+            EventType.STLG => Converter.BytesToPacket<StartLights>(remainingPacket),
+            EventType.LGOT => Converter.BytesToPacket<StartLights>(remainingPacket),
+            EventType.SPTP => Converter.BytesToPacket<SpeedTrap>(remainingPacket),
+            EventType.FTLP => Converter.BytesToPacket<FastestLap>(remainingPacket),
+            EventType.TMPT => Converter.BytesToPacket<TeamMateInPits>(remainingPacket),
+            EventType.RCWN => Converter.BytesToPacket<RaceWinner>(remainingPacket),
+            EventType.RTMT => Converter.BytesToPacket<Retirement>(remainingPacket),
+            EventType.PENA => Converter.BytesToPacket<Penalty>(remainingPacket),
+            EventType.SGSV => Converter.BytesToPacket<StopGoPenaltyServed>(remainingPacket),
+            EventType.DTSV => Converter.BytesToPacket<DriveThroughPenaltyServed>(remainingPacket),
+            _ => null,
+        };
     }
 
     public override void RaiseEventHandler(PacketId id, byte[] remainingPacket)
