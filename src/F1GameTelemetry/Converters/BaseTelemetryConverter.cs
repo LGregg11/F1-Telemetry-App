cf183ae04ae0469ec78e090f225c714187b5e019
Luke Gregory <L.Gregory11@hotmail.co.uk>
2023-07-07 19:57:51 +0100

    F1-33: Create singleton telemetry reader class

---
+++ src/F1GameTelemetry/Converters/BaseTelemetryConverter.cs
@@ -0,0 +1,44 @@
+﻿namespace F1GameTelemetry.Converters;
+
+using F1GameTelemetry.Enums;
+using F1GameTelemetry.Packets.Standard;
+
+using System;
+using System.Linq;
+using System.Text;
+
+public abstract class BaseTelemetryConverter : ITelemetryConverter
+{
+    public BaseTelemetryConverter()
+    {
+        IsExportEnabled = false;
+    }
+
+    public abstract string Name { get; }
+    public abstract bool IsSupported { get; }
+    public abstract GameVersion GameVersion { get; }
+
+    public bool IsExportEnabled { get; set; }
+    public int MaxCarsPerRace => 22;
+    public int HeaderPacketSize => 24;
+
+    public abstract Header ConvertBytesToHeader(byte[] bytes);
+
+    public abstract object? ConvertBytesToStandardPacket(PacketId packetType, byte[] bytes);
+
+    public EventType GetEventType(byte[] remainingPacket)
+    {
+        try
+        {
+            return (EventType)Enum.Parse(
+                typeof(EventType),
+                Encoding.ASCII.GetString(remainingPacket.Take(4).ToArray())
+                );
+        }
+        catch
+        {
+            // Return an unknown event type instead of an error
+            return EventType.UNKNOWN;
+        }
+    }
+}
---
+++ src/F1GameTelemetry/Converters/ITelemetryConverter.cs
@@ -0,0 +1,16 @@
+﻿namespace F1GameTelemetry.Converters;
+
+using F1GameTelemetry.Enums;
+using F1GameTelemetry.Packets.Standard;
+
+public interface ITelemetryConverter
+{
+    string Name { get; }
+    GameVersion GameVersion { get; }
+    bool IsSupported { get; }
+    bool IsExportEnabled { get; }
+    int MaxCarsPerRace { get; }
+    int HeaderPacketSize { get; }
+    Header ConvertBytesToHeader(byte[] bytes);
+    object? ConvertBytesToStandardPacket(PacketId packetType, byte[] bytes);
+}
