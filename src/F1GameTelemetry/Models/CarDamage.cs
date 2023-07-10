a3f21854a478672eb14eebb1ee21f23bdab94833
Luke Gregory <L.Gregory11@hotmail.co.uk>
2023-07-10 21:01:54 +0100

    F1-57: Created simplified struct models to replace standard structs

---
+++ src/F1GameTelemetry/Models/CarDamage.cs
@@ -0,0 +1,41 @@
+﻿namespace F1GameTelemetry.Models;
+
+
+public struct CarDamage
+{
+    public CarDamage(CarDamageData[] carDamageData)
+    {
+        this.carDamageData = carDamageData;
+    }
+
+    public CarDamageData[] carDamageData;
+}
+
+public struct CarDamageData
+{
+    public CarDamageData(
+        FourAxleFloat tyreWear,
+        FourAxleByte tyreDamage,
+        FourAxleByte brakeDamage,
+        byte frontLeftWingDamage,
+        byte frontRightWingDamage,
+        byte rearWingDamage)
+    {
+        this.tyreWear = tyreWear;
+        this.tyreDamage = tyreDamage;
+        this.brakeDamage = brakeDamage;
+        this.frontLeftWingDamage = frontLeftWingDamage;
+        this.frontRightWingDamage = frontRightWingDamage;
+        this.rearWingDamage = rearWingDamage;
+    }
+
+    // All properties are percentage
+
+    public FourAxleFloat tyreWear;
+    public FourAxleByte tyreDamage;
+    public FourAxleByte brakeDamage;
+
+    public byte frontLeftWingDamage;
+    public byte frontRightWingDamage;
+    public byte rearWingDamage;
+}
--- src/F1GameTelemetry/Models/SessionHistory.cs
+++ src/F1GameTelemetry/Models/SessionHistory.cs
@@ -1,10 +1,8 @@
-﻿namespace F1GameTelemetry.Packets.Standard;
+﻿namespace F1GameTelemetry.Models;
 
 using Enums;
 
-using System.Runtime.InteropServices;
 
-[StructLayout(LayoutKind.Sequential, Pack = 1, Size = 1131)]
 public struct SessionHistory
 {
     public SessionHistory(
@@ -36,30 +34,23 @@ public struct SessionHistory
     public byte bestSector1LapNum;
     public byte bestSector2LapNum;
     public byte bestSector3LapNum;
-
-    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 100)]
     public LapHistoryData[] lapHistoryData;
-
-    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
     public TyreStintHistoryData[] tyreStintHistoryData;
 
 }
 
-[StructLayout(LayoutKind.Sequential, Pack = 1, Size = 11)]
 public struct LapHistoryData
 {
     public LapHistoryData(
         uint lapTime,
         ushort sector1Time,
         ushort sector2Time,
-        ushort sector3Time,
-        byte lapValidBitFlags)
+        ushort sector3Time)
     {
         this.lapTime = lapTime;
         this.sector1Time = sector1Time;
         this.sector2Time = sector2Time;
         this.sector3Time = sector3Time;
-        this.lapValidBitFlags = lapValidBitFlags;
     }
 
     // All times in milliseconds
@@ -67,23 +58,18 @@ public struct LapHistoryData
     public ushort sector1Time;
     public ushort sector2Time;
     public ushort sector3Time;
-    public byte lapValidBitFlags; // 0x01 bit set-lap valid, 0x02 bit set-sector 1 valid, 0x04 bit set-sector 2 valid, 0x08 bit set-sector 3 valid
 }
 
-[StructLayout(LayoutKind.Sequential, Pack = 1, Size = 3)]
 public struct TyreStintHistoryData
 {
     public TyreStintHistoryData(
         byte endLap,
-        TyreCompoundType tyreActualCompound,
         TyreVisualType tyreVisualCompound)
     {
         this.endLap = endLap;
-        this.tyreActualCompound = tyreActualCompound;
         this.tyreVisualCompound = tyreVisualCompound;
     }
 
     public byte endLap; // 255 of current tyre
-    public TyreCompoundType tyreActualCompound;
     public TyreVisualType tyreVisualCompound;
 }
