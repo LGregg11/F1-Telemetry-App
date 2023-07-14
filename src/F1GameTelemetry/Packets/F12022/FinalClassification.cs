c25a1d520012eabb95133dc09a009c1d5e1a01cf
Luke Gregory <L.Gregory11@hotmail.co.uk>
2023-07-14 19:20:15 +0100

    F1-58: Added F1 22 packets
    - Marked new fields with a comment

---
+++ src/F1GameTelemetry/Packets/F12022/FinalClassification.cs
@@ -0,0 +1,39 @@
+﻿namespace F1GameTelemetry.Packets.F12022;
+
+using Enums;
+
+using System.Runtime.InteropServices;
+
+[StructLayout(LayoutKind.Sequential, Pack = 1, Size = 990)]
+public struct FinalClassification
+{
+    public byte numberCars;
+
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 22)]
+    public FinalClassificationData[] finalClassificationData;
+}
+
+[StructLayout(LayoutKind.Sequential, Pack = 1, Size = 45)]
+public struct FinalClassificationData
+{
+    public byte position;
+    public byte numberLaps;
+    public byte gridPosition;
+    public byte points;
+    public byte numberPitStops;
+    public ResultStatus resultStatus;
+    public uint bestLapTime; // In milliseconds
+    public double totalRaceTime; // In seconds without penalities
+    public byte penalitesTime; // Total in seconds
+    public byte numberPenalties;
+    public byte numberTyreStints;
+
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
+    public TyreCompoundType[] tyreStintsActual;
+
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
+    public TyreVisualType[] tyreStintsVisual;
+
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
+    public byte[] tyreStintsEndLaps; // New to 2022 - The lap number stints end on
+}
