323a8f4d45ede1d29500ccf339bdeb90299c814a
Luke Gregory <L.Gregory11@hotmail.co.uk>
2023-07-14 20:09:00 +0100

    F1-58: Added F1 23 packets
    - Marked new fields with a comment

---
+++ src/F1GameTelemetry/Packets/F12023/CarDamage.cs
@@ -0,0 +1,46 @@
+﻿namespace F1GameTelemetry.Packets.F12023;
+
+using Enums;
+
+using System.Runtime.InteropServices;
+
+[StructLayout(LayoutKind.Sequential, Pack = 1, Size = 924)]
+public struct CarDamage
+{
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 22)]
+    public CarDamageData[] carDamageData;
+}
+
+[StructLayout(LayoutKind.Sequential, Pack = 1, Size = 42)]
+public struct CarDamageData
+{
+    // All non-enum properties are percentage unless specified otherwise
+
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+    public float[] tyreWear;
+
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+    public byte[] tyreDamage;
+
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+    public byte[] brakeDamage;
+
+    public byte frontLeftWingDamage;
+    public byte frontRightWingDamage;
+    public byte rearWingDamage;
+    public byte floorDamage;
+    public byte diffuserDamage;
+    public byte sidepodDamage;
+    public Fault drsFault;
+    public Fault ersFault; // New to 2022
+    public byte gearBoxDamage;
+    public byte engineDamage;
+    public byte engineMGUHWear;
+    public byte engineESWear;
+    public byte engineCEWear;
+    public byte engineICEWear;
+    public byte engineMGUKWear;
+    public byte engineICWear;
+    public Fault engineBlown; // New to 2022
+    public Fault engineSeized; // New to 2022
+}
